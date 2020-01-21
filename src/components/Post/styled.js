c8ba4aa78d66ddf9d20d3a30847f704d0b97233b
Luiz Correia <luiz@luizcorreia.eti.br>
2020-01-21 20:21:27 -0300

    Update feature Image

--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import media from 'styled-media-query'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import Img from 'gatsby-image'
 
 import transitions from '../../styles/transitions';
 
