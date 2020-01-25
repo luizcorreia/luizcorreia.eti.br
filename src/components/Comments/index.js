ed16af1bea0875de4df92fd3274d1b9ca2031b56
Luiz Correia <luiz@luizcorreia.eti.br>
2020-01-25 12:34:02 -0300

    Change shortname disgus

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="luiz__correia"
+        shortname="luizcorreia"
         identifier={completeURL}
         title={title}
         url={completeURL}
